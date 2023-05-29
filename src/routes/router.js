069216160f1561cac43b86738cba7223e33492a2
yocoendi78 <92427812+yocoendi@users.noreply.github.com>
2023-05-29 21:34:11 +0200

    mejorado

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,68 +1,64 @@
-import { Router } from 'express';
+import express from 'express';
 import { pool1 } from '../db.js';
 import { vistaHome, vistaLogin, vistaRegistro, vistaSuscribirse, postMetodo } from '../controller/indexRoutex.js';
 import { PrismaClient } from '@prisma/client';
 import bcryptjs from 'bcryptjs';
 
-const router = Router()
+const router = express.Router();
 
-
-
-// Crear nuestras rutas para las diferentes paginas
-router.get('/', vistaHome)
-router.get('/login', vistaLogin)
-router.get('/registro', vistaRegistro)
-router.get('/suscribirse', vistaSuscribirse)
-router.post('/', postMetodo)
+// Crear nuestras rutas para las diferentes páginas
+router.get('/', vistaHome);
+router.get('/login', vistaLogin);
+router.get('/registro', vistaRegistro);
+router.get('/suscribirse', vistaSuscribirse);
+router.post('/', postMetodo);
 
 router.post('/auth', async (req, res) => {
+  try {
     const user = req.body.user;
     const pass = req.body.pass;
     if (user && pass) {
-        try {
-            const [results] = await pool1.query('SELECT * from Users where user = ?', [user]);
-            if (results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))) {
-                res.send('Usuario o contraseña incorrectos')
-            } else {
-                res.send('Login correcto')
-            }
-        } catch (error) {
-
-        }
+      const [results] = await pool1.query('SELECT * from Users where user = ?', [user]);
+      if (results.length === 0 || !(await bcryptjs.compare(pass, results[0].pass))) {
+        res.status(401).send('Usuario o contraseña incorrectos');
+      } else {
+        res.status(200).send('Login correcto');
+      }
+    } else {
+      res.status(400).send('Faltan campos obligatorios');
     }
-})
+  } catch (error) {
+    res.status(500).send('Error en el servidor');
+  }
+});
 
 router.post('/registro', async (req, res) => {
+  try {
     const name = req.body.name;
     const surname = req.body.surname;
     const user = req.body.user;
     const email = req.body.email;
     const pass = req.body.pass;
-    let passwordHash = await bcryptjs.hash(pass, 8);
-    pool1.query('INSERT INTO Users SET ?', { name: name, surname: surname, email: email, pass: passwordHash, user: user, })
-        .then(() => {
-            res.send('Usuario insertado correctamente')
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+    const passwordHash = await bcryptjs.hash(pass, 8);
+    await pool1.query('INSERT INTO Users SET ?', { name, surname, email, pass: passwordHash, user });
+    res.status(200).send('Usuario insertado correctamente');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error en el servidor');
+  }
 });
 
 router.post('/suscribirse', async (req, res) => {
+  try {
     const name = req.body.name;
     const surname = req.body.surname;
     const email = req.body.email;
-    pool1.query('INSERT INTO Users SET ?', { name: name, surname: surname, email: email})
-        .then(() => {
-            res.send('Suscripción correcta')
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+    await pool1.query('INSERT INTO Users SET ?', { name, surname, email });
+    res.status(200).send('Suscripción correcta');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error en el servidor');
+  }
 });
 
-
-
-
 export default router;
-
