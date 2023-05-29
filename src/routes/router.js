b8373985c16ca9e3d28ed65b6e80de0af24e6c72
yocoendi78 <92427812+yocoendi@users.noreply.github.com>
2023-05-29 19:41:56 +0200

    d

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { pool1 } from '../db.js';
-import { vistaHome, vistaLogin, vistaRegistro, postMetodo } from '../controller/indexRoutex.js';
+import { vistaHome, vistaLogin, vistaRegistro, vistaSuscribirse, postMetodo } from '../controller/indexRoutex.js';
 import { PrismaClient } from '@prisma/client';
 import bcryptjs from 'bcryptjs';
 
@@ -12,6 +12,7 @@ const router = Router()
 router.get('/', vistaHome)
 router.get('/login', vistaLogin)
 router.get('/registro', vistaRegistro)
+router.get('/suscribirse', vistaSuscribirse)
 router.post('/', postMetodo)
 
 router.post('/auth', async (req, res) => {
@@ -39,6 +40,19 @@ router.post('/registro', async (req, res) => {
     const pass = req.body.pass;
     let passwordHash = await bcryptjs.hash(pass, 8);
     pool1.query('INSERT INTO Users SET ?', { name: name, surname: surname, email: email, pass: passwordHash, user: user, })
+        .then(() => {
+            res.send('Usuario insertado correctamente')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+});
+
+router.post('/suscribirse', async (req, res) => {
+    const name = req.body.name;
+    const surname = req.body.surname;
+    const email = req.body.email;
+    pool1.query('INSERT INTO Users SET ?', { name: name, surname: surname, email: email})
         .then(() => {
             res.send('Usuario insertado correctamente')
         })
